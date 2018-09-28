4f2a7135371d7c73fe1b08c8fdf7a56f8e5d3a6c
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-28 13:29:20 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  // создали массив из элементов str;
-  var arr1 = str.split('');
-
-  var brackDif = [];
-  var brackSame = [];
-  for (var i = 0; i < bracketsConfig.length; i++){
+  const arr = str.split('');
+  let brackDif = [];
+  let brackSame = [];
+  for (let i = 0; i < bracketsConfig.length; i++){
     if (bracketsConfig[i][0] !== bracketsConfig[i][1]){
       brackDif.push(bracketsConfig[i]);
     }
@@ -14,45 +12,38 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
-  var arrOp = [arr1[0]];
-  arrMain: for (var i = 1; i < arr1.length; i++){
-    for (var j = 0; j < brackDif.length; j++){
-      if (arr1[i] == brackDif[j][1]){
+  let arrOp = [arr[0]];
+  arrMain: for (let i = 1; i < arr.length; i++){
+    for (let j = 0; j < brackDif.length; j++){
+      if (arr[i] == brackDif[j][1]){
         if (arrOp[arrOp.length - 1] == brackDif[j][0]){
           arrOp.pop();
-          // тут переход на след i
           continue arrMain;
-
         }
         else {
           return false;
         }
       }
     }
-
-    for (var j = 0; j < brackSame.length; j++){
-      if (arr1[i] == brackSame[j][1]){
-        if (arrOp[arrOp.length - 1] == arr1[i]){
+    for (let j = 0; j < brackSame.length; j++){
+      if (arr[i] == brackSame[j][1]){
+        if (arrOp[arrOp.length - 1] == arr[i]){
           arrOp.pop();
-          continue arrMain;
         }
         else {
-          arrOp.push(arr1[i]);
-          continue arrMain;
+          arrOp.push(arr[i]);
         }
+        continue arrMain;
       }
     }
-
-    for (var j = 0; j < brackDif.length; j++){
-      if (arr1[i] == brackDif[j][0]){
-        arrOp.push(arr1[i]);
+    for (let j = 0; j < brackDif.length; j++){
+      if (arr[i] == brackDif[j][0]){
+        arrOp.push(arr[i]);
         continue arrMain;
       }
     }
   }
 
-  if (arrOp.length > 0){
-    return false;
-  }
-  else return true;
+  if (arrOp.length === 0) return true 
+  else return false;
 }
